a3b4de0e2883272b9f6a100dcc9adb46bc2fbdae
Elaine <elaine.almeida@al.infnet.edu.br>
2024-06-09 01:01:16 -0300

    Curso e Mentoria

---
+++ src/Main.java
@@ -0,0 +1,37 @@
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args) {
+        Curso curso1 = new Curso();
+
+        curso1.setTitulo("Curso Java");
+        curso1.setDescricao("Descrição do curso de Java");
+        curso1.setCargaHoraria(8);
+        System.out.println(curso1);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("Curso JavaScript");
+        curso2.setDescricao("Descrição do curso de JavaScript");
+        curso2.setCargaHoraria(8);
+        System.out.println(curso2);
+
+        Mentoria mentoria1 = new Mentoria();
+        mentoria1.setTitulo("Mentoria Java");
+        mentoria1.setDescricao("Descrição mentoria de Java");
+        mentoria1.setData(LocalDate.now());
+        System.out.println(mentoria1);
+
+        Mentoria mentoria2 = new Mentoria();
+        mentoria2.setTitulo("Mentoria JavaScript");
+        mentoria2.setDescricao("Descrição mentoria de JavaScript");
+        mentoria2.setData(LocalDate.now());
+        System.out.println(mentoria2);
+
+
+
+
+    }
+}
